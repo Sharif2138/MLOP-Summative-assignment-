81834436ef705134e432aa62788810e6776f034a
Sharif2138 <s.kiviiri@alustudent.com>
2026-04-02 10:26:25 +0200

    added plot visualisation in stream_lit.py

--- ui/streamlit_app.py
+++ ui/streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import os
-from scripts.upload_new_data import upload_new_data
+from api.upload_new_data import upload_new_data
 import requests
 
 API_URL = "http://127.0.0.1:8000"
@@ -33,6 +33,45 @@ if uploaded_file is not None:
             st.info(f"Confidence: {result['confidence']:.4f}")
         else:
             st.error("Prediction failed")
+            
+# plots
+st.header("Model Performance Dashboard")
+
+plot_files = {
+    "class_distribution": "plots/class_distribution.png",
+    "Training Accuracy": "plots/accuracy_curve.png",
+    "Training Loss": "plots/loss_curve.png",
+    "confusion_matrix": "plots/confusion_matrix.png",
+}
+
+# Check if at least one plot exists
+available_plots = [p for p in plot_files.values() if os.path.exists(p)]
+
+if available_plots:
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        if os.path.exists(plot_files["class_distribution"]):
+            st.image(plot_files["class_distribution"],
+                     caption="Class Distribution", use_container_width=True)
+
+        if os.path.exists(plot_files["Training Accuracy"]):
+            st.image(plot_files["Training Accuracy"],
+                     caption="Training Accuracy", use_container_width=True)
+
+    with col2:
+        
+        if os.path.exists(plot_files["Training Loss"]):
+            st.image(plot_files["confusion_matrix"],
+                     caption="Confusion Matrix", use_container_width=True)
+        
+        if os.path.exists(plot_files["confusion_matrix"]):
+            st.image(plot_files["confusion_matrix"],
+                     caption="Confusion Matrix", use_container_width=True)
+
+else:
+    st.info("No training plots found. Train the model to generate performance metrics.")
 
 
 
